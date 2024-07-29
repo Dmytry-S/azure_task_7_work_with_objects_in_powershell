07732206ab9e60556aa3089e417fb9a88295ec42
Dmytry-S <sviridoffd@gmail.com>
2024-07-29 18:54:15 +0300

    Implement task 7

--- task.ps1
+++ task.ps1
@@ -1,1 +1,16 @@
 # Write your code here
+
+$pathToData = "./data"
+$vmSize = "Standard_B2pts_v2"
+$regionsWithSize = @()
+
+$jsonFiles = Get-ChildItem -Path $pathToData -Filter "*.json"
+foreach ($file in $jsonFiles) {
+    $pathToFile = $file.FullName
+    $vmTypes = Get-Content -Path $pathToFile | ConvertFrom-Json
+    if ($vmTypes.Name -contains $vmSize) {
+        $regionName = $file.BaseName.Replace('.json', '')
+        $regionsWithSize += $regionName
+    }
+}
+$regionsWithSize | ConvertTo-Json | Set-Content -Path "./result.json"
